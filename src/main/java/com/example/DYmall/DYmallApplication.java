73e80b22cd7e058953f961e80b00a3d403785759
yulim <yulimlvphs@gmail.com>
2023-03-29 18:42:00 +0900

    first commit

---
+++ src/main/java/com/example/DYmall/DYmallApplication.java
@@ -0,0 +1,13 @@
+package com.example.DYmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DYmallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DYmallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/DYmall/DYmallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DYmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DYmallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
