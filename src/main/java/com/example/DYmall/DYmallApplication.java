cf1c8ba8a65544a24093adf91feeaff5be4e4343
seodong-geun <mogijisung13@gmail.com>
2023-05-03 15:35:09 +0900

    [05.03(수)] - 엔티티 생성, 수정일자 자동처리 어노테이션 추가

--- src/main/java/com/example/DYmall/DYmallApplication.java
+++ src/main/java/com/example/DYmall/DYmallApplication.java
@@ -2,8 +2,10 @@ package com.example.DYmall;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing // 엔티티에 대한 생성일자, 수정일자 정보를 자동으로 처리하기 위한 애노테이션
 public class DYmallApplication {
 
 	public static void main(String[] args) {
