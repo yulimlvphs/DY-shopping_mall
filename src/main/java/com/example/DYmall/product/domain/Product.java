2fb427a5c0b46bcec3d8eb5f768b85d43f8f10f0
seodong-geun <mogijisung13@gmail.com>
2023-05-09 17:26:03 +0900

    [05.09(수)] - 상품 엔티티 필드 수정 (감소)

--- src/main/java/com/example/DYmall/product/domain/Product.java
+++ src/main/java/com/example/DYmall/product/domain/Product.java
@@ -9,35 +9,25 @@ import javax.persistence.*;
  * @since 2023/04/10
  */
 @Entity // JPA 엔티티 클래스임을 나타냄
-@Getter
-@Builder
-@NoArgsConstructor // 파라미터가 없는 기본 생성자를 자동으로 생성
-@AllArgsConstructor // 모든 필드를 인자로 받는 생성자를 자동으로 생성
-@ToString
+@Data
 @Table(name = "product")
 public class Product extends BaseEntity{
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY) // 기본키 자동 생성
-    private Long id; // 상품 테이블 기본키(id)
+    @Column(name = "product_id")
+    @GeneratedValue(strategy = GenerationType.AUTO) // 기본키 자동 생성
+    private Long id; // 상품코드
 
     @Column(length = 200, nullable = false)
     private String name; // 상품명
 
-    @Column(length = 1500,nullable = false)
-    private String description; // 상품설명
-
     @Column(nullable = false)
     private int price; // 상품가격
+
+    @Column(length = 1500, nullable = false)
+    private String description; // 상품설명
 
     @Column(nullable = false)
     private int stock; // 재고
 
-    @Column(nullable = false)
-    private String category; // 상품 카테고리
-
-    @Column(nullable = false)
-    private String brand; // 상품 브랜드
-
-    // 생성자, getters, setters
 }
--- src/main/java/com/example/DYmall/product/dto/ProductDto.java
+++ src/main/java/com/example/DYmall/product/dto/ProductDto.java
@@ -11,17 +11,12 @@ import java.time.LocalDateTime;
  */
 @Getter // Getter 메소드 자동생성
 @Setter // Setter 메소드 자동생성
-@NoArgsConstructor // 파라미터가 없는 기본 생성자 자동생성
-@AllArgsConstructor // 모든 필드를 인자로 받는 생성자 자동생성
-@Builder // 필드가 많아져도 객체 생성시 가독성이 좋아지는 빌터 패턴 자동생성
 public class ProductDto {
     private Long id;
     private String name;
+    private Integer price;
     private String description;
-    private int price;
-    private int stock;
-    private String category;
-    private String brand;
+    private Integer stock;
     private LocalDateTime regDate, modDate;
 }
 
