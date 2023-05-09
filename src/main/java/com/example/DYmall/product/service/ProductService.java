fdf911b36550388ed487126a0e87f6dfad53bc60
seodong-geun <mogijisung13@gmail.com>
2023-05-09 17:23:27 +0900

    [05.09(수)] - 상품 서비스 클래스(인터페이스, 구현객체)
    > 상품 등록 기능 구현 완료

--- src/main/java/com/example/DYmall/product/service/ProductService.java
+++ src/main/java/com/example/DYmall/product/service/ProductService.java
@@ -1,7 +1,7 @@
 package com.example.DYmall.product.service;
 
-import com.example.DYmall.product.domain.Product;
-import com.example.DYmall.product.dto.ProductDto;
+import com.example.DYmall.product.dto.ProductFormDto;
+
 
 
 /**
@@ -11,32 +11,6 @@ import com.example.DYmall.product.dto.ProductDto;
  */
 public interface ProductService {
 
-    /**
-     * 상품 정보 수정 기능
-     * @param productDto 상품 정보 DTO
-     * @return 수정된 상품 ID
-     */
-    Long updateItem(ProductDto productDto);
-
-
-    /**
-     * 상품 정보 DTO 객체를 상품 엔티티 객체로 변환하는 기능
-     * @param dto 상품 정보 DTO
-     * @return 상품 엔티티
-     */
-    // Java 8부터는 디폴트 메소드 인터페이스에 작성 시 구현객체에서 바로 사용가능
-    default Product dtoToEntity(ProductDto dto) {
-        // Product의 객체를 builder로 생성 후 entity에 저장후 반환
-        Product entity = Product.builder()
-                .id(dto.getId())
-                .name(dto.getName())
-                .description(dto.getDescription())
-                .price(dto.getPrice())
-                .stock(dto.getStock())
-                .category(dto.getCategory())
-                .brand(dto.getBrand())
-                .build();
-        return entity;
-    }
+    Long saveProduct(ProductFormDto productFormDto);
 }
 
