81f6ade9766d7de4d1f343e6b7bba298321a0418
seodong-geun <mogijisung13@gmail.com>
2023-05-10 13:23:31 +0900

    [05.10(수)] - 주석 작성 완료

--- src/main/java/com/example/DYmall/product/dto/ProductFormDto.java
+++ src/main/java/com/example/DYmall/product/dto/ProductFormDto.java
@@ -13,11 +13,14 @@ import javax.validation.constraints.NotNull;
  * @version 1.1
  * @Author seodong-geun
  * @since 2023/05/09
+ * 상품 정보를 담은 폼(form) 데이터를 담는 DTO 클래스
  */
 @Getter
 @Setter
 public class ProductFormDto {
 
+    // modelMapper 객체 생성
+    // Java Bean을 이용하여 객체간의 매핑을 쉽게 처리할 수 있도록 도와주는 라이브러리
     private static ModelMapper modelMapper = new ModelMapper();
     private Long id;
 
@@ -33,10 +36,12 @@ public class ProductFormDto {
     @NotNull(message = "상품 재고는 필수 입력 값입니다.")
     private Integer stock;
 
+    // ProductFormDto 객체를 Product 객체로 변환하여 반환
     public Product createProduct(){
         return modelMapper.map(this, Product.class);
     }
 
+    // Product 객체를 ProductFormDto 객체로 변환하여 반환
     public static ProductFormDto of(Product product){
         return modelMapper.map(product, ProductFormDto.class);
     }
